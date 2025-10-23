46e56807d6693ed19a37f22a716460ce11c4ed54
phaethon90 <phaethon.ljc@gmail.com>
2025-10-23 19:33:20 +0800

    feat: svg-icon设置图标

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 'off',//关闭多单词命名规范
+    'vue/multi-word-component-names': 'off', //关闭多单词命名规范
     // 防止eslint与prettierrc冲突,添加以下两行代码
     indent: 0,
     'space-before-function-paren': 0
