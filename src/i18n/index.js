6fce537f10e4a23a099ab273c11af76b4b17f3e7
phaethon90 <phaethon.ljc@gmail.com>
2025-11-06 20:32:06 +0800

    feat: i18n初使用（但提示翻译函数没有找到对应的翻译键，暂未解决，先跳过国际化）

---
+++ src/i18n/index.js
@@ -0,0 +1,30 @@
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+  en: {
+    msg: {
+      title: 'user login'
+    }
+  },
+  zh: {
+    msg: {
+      title: '用户登录'
+    }
+  }
+}
+
+const getCurrentLanguage = () => {
+  const UAlang = navigator.language //获取浏览器语言
+  const langCode = UAlang.indexOf('zh') !== -1 ? 'zh' : 'en'
+  localStorage.setItem('lang', langCode)
+  return langCode
+}
+
+const i18n = createI18n({
+  legacy: false, //使用 Composition API 模式
+  globalInjection: true, //全局注入 $t 方法
+  locale: getCurrentLanguage() || 'zh', //当前语言
+  messages: messages
+})
+
+export default i18n
--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import Svgicon from '@/icons'
 import 'element-plus/dist/index.css'
 import '@/router/permission'
 import * as ELIcons from '@element-plus/icons-vue'
+import i18n from '@/i18n'
 
 const app = createApp(App)
 for (const iconName in ELIcons) {
   app.component(iconName, ELIcons[iconName])
 }
 Svgicon(app)
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(i18n).mount('#app')
